bba1d4e62965adf4afeecb04b784ea83b13bcab8
abdifatah-bashi <abdifatah.bashi@nav.no>
2024-09-11 12:14:36 +0200

    Legger til Rapportering Fabrikk schema

--- src/schemas/index.tsx
+++ src/schemas/index.tsx
@@ -3,10 +3,12 @@ import { innsendingFerdigstilt } from './innsending_ferdigstilt'
 import { RapporteringspliktDato } from './rapporteringsplikt_dato'
 import { OrkestratorBehov } from './orkestrator-behov'
 import { RJSFSchema } from '@rjsf/utils'
+import { RapporteringFabrikk } from './rapportering-fabrikk'
 
 export const schemas: RJSFSchema[] = [
   Test,
   innsendingFerdigstilt,
   RapporteringspliktDato,
   OrkestratorBehov,
+  RapporteringFabrikk,
 ]
---
+++ src/schemas/rapportering-fabrikk.ts
@@ -0,0 +1,30 @@
+import { RJSFSchema } from '@rjsf/utils'
+
+export const RapporteringFabrikk: RJSFSchema = {
+  title: 'Rapportering Fabrikk',
+  type: 'object',
+  properties: {
+    '@event_name': {
+      type: 'string',
+      const: 'behov',
+      default: 'behov',
+      readOnly: true,
+    },
+    '@behov': {
+      type: 'string',
+      const: 'ny_rapportering',
+      default: 'ny_rapportering',
+    },
+
+    ident: {
+      type: 'string',
+    },
+    fraOgMed: {
+      type: 'string',
+    },
+    behovId: {
+      type: 'string',
+    },
+  },
+  required: ['@event_name', '@behov', 'ident', 'fraOgMed', 'behovId'],
+}
