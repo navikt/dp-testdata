3471ac3cb63aecbacae9b85f06f9a720de1d241b
abdifatah-bashi <abdifatah.bashi@nav.no>
2025-05-20 10:39:16 +0200

    Legg flere felter til VedtakFattet-Schema

--- src/schemas/index.tsx
+++ src/schemas/index.tsx
@@ -3,10 +3,6 @@ import { innsendingFerdigstilt } from './innsending_ferdigstilt'
 import { OrkestratorBehov } from './orkestrator-behov'
 import { RJSFSchema } from '@rjsf/utils'
 import { VedtakFattet } from './rapportering-fabrikk'
-import {
-  PersonregisterSøknad,
-  PersonregisterVedtak,
-} from './rapportering-personregister'
 
 export const schemas: RJSFSchema[] = [
   Test,
--- src/schemas/rapportering-fabrikk.ts
+++ src/schemas/rapportering-fabrikk.ts
@@ -1,4 +1,5 @@
 import { RJSFSchema } from '@rjsf/utils'
+import { format } from 'path'
 
 export const VedtakFattet: RJSFSchema = {
   title: 'Vedtak fattet',
@@ -10,17 +11,48 @@ export const VedtakFattet: RJSFSchema = {
       default: 'vedtak_fattet',
       readOnly: true,
     },
+    behandlingId: {
+      type: 'string',
+      default: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+    },
+    vedtakstidspunkt: {
+      type: 'string',
+      format: 'date-time',
+    },
     ident: {
       type: 'string',
     },
-    gyldigFraOgMed: {
-      type: 'string',
-      default: '2025-05-20',
-    },
-    gyldigTilOgMed: {
-      type: 'string',
-      default: '2026-05-20',
+
+    opplysninger: {
+      type: 'array',
+      title: 'Opplysninger',
+      minItems: 1,
+      maxItems: 1,
+
+      items: {
+        type: 'object',
+        title: '',
+        properties: {
+          gyldigFraOgMed: {
+            type: 'string',
+            format: 'date',
+            title: 'Gyldig fra og med',
+          },
+          gyldigTilOgMed: {
+            type: 'string',
+            format: 'date',
+            title: 'Gyldig til og med',
+          },
+        },
+        required: ['gyldigFraOgMed', 'gyldigTilOgMed'],
+      },
     },
   },
-  required: ['@event_name', 'ident', 'gyldigFraOgMed', 'gyldigTilOgMed'],
+  required: [
+    '@event_name',
+    'behandlingId',
+    'vedtakstidspunkt',
+    'opplysninger',
+    'ident',
+  ],
 }
